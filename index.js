adb80df092b7d6af1fc8decef3700c1f06748a43
Kartheek Dabbiru <dabbiru.k@northeastern.edu>
2022-02-09 15:46:49 -0500

    Basic API call

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+
+app.get("/healthz", (req, res, next) => {
+    res.json("You have reached the /healthz endpoint");
+    res.status(200);
+   });
+   
+
+app.listen(3000, () => {
+ console.log("Welcome to the web service for CSYE6225 : Listening on port 3000");
+});
