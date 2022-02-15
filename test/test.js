293956b766adf3cfbd9f857f3cd22dc2c5223f8a
Kartheek Dabbiru <dabbiru.k@northeastern.edu>
2022-02-15 18:49:17 -0500

    demo

--- test/test.js
+++ test/test.js
@@ -15,7 +15,7 @@ describe("UNIT TEST for healthz api endpoint",function(){
     .expect(200) // THis is HTTP response
     .end(function(err,res){
       // HTTP status should be 200
-      res.status.should.equal(404);
+      res.status.should.equal(200);
       // Error key should be false.
     //   res.body.error.should.equal(false);
       done();
