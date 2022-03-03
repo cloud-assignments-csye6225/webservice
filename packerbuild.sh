70b7944838751fc98611ad9ca6baa9345f1a57a2
Kartheek Dabbiru <dabbiru.k@northeastern.edu>
2022-03-03 00:54:39 -0500

    script cleanup

--- packerbuild.sh
+++ packerbuild.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum upgrade -y
-sudo yum install nginx -y
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo yum install nodejs -y
+sleep 30
+sudo yum -y update
+sudo yum install -y zip unzip
+sleep 30
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum install -y nodejs
 cd /tmp
 unzip app_patch.zip
 cd app_patch
+sudo npm install bcrypt
 sudo npm install
+sudo npm install -g nodemon
